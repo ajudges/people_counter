7db457a1fec3eac1d69ca0cb80dc712402c49d7a
ajudges <ajudges@gmail.com>
2020-06-08 08:35:26 +0200

    Added Mac support in script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing nodejs, modules required for mqtt server and ffmpeg"
+
 # function for linux setup
 function install_Nodejs_and_dependencies{
     echo "Nodejs and its dependencies"
@@ -43,6 +45,38 @@ function install_npm{
 function install ffmpeg{
 npm install ffmpeg
 }
+
+# Mac function to install nodejs
+function mac_install_nodejs {
+    # update brew
+    brew update
+    brew doctor
+    # installing node and npm
+    brew install node
+    # print the version of node installed
+    node -v
+    #print the version of npm installed
+    npm -v
+}
+
+# Mac function to install modules
+function mac_install_dep {
+    pip3 install numpy
+    pip3 install paho-mqtt
+    brew install cmake
+    brew install zeromq
+}
+
+# Mac function to install FFmpeg's ffserver
+function mac_install_ff {
+    git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
+    cd ffmpeg
+    git checkout 2ca65fc7b74444edd51d5803a2c1e05a801a6023
+    ./configure
+    make -j4
+}
+
+
 # obtain  os type
 p_name=`uname`
 echo $p_name
@@ -62,4 +96,30 @@ then
     
 #   install ffmpeg
     install ffmpeg
+elif [ "$p_name" == 'Darwin']
+then
+    echo "I am a Mac"
+    # Return 1 if python version is ≥ 3.5, otherwise return 0 if python < 3.5 or doesn't exist.
+    py_version=$(python -c"import sys; print(0) if sys.version_info.major < 3 and sys.version_info.minor < 5 else print(1)")
+    # if python version is ≥ 3.5, do
+    if [ $py_version == 1 ]
+    then
+        # install nodejs
+        mac_install_nodejs
+        # install dependencies
+        mac_install_dep
+        # install ffserver
+        mac_install_ff
+
+    # if python version is < 3.5 or python isn't installed
+    else
+        # use brew to install python
+        brew install python3
+        # install nodejs
+        mac_install_nodejs
+        # install dependencies
+        mac_install_dep
+        # install ffserver
+        mac_install_ff
+    fi
 fi    
